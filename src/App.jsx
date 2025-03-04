fe18c13412917de51edcc92021a00f77ec93bf08
sajid-edninja <ssk73573@gmail.com>
2025-03-04 23:51:53 +0530

    enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className="grid grid-cols-12" >
-        <div className="col-span-12 sm:col-span-5 bg-green-300">First</div>
+        <div className="col-span-12 sm:col-span-5 bg-green-300 text-8xl rounded-full">First</div>
         <div className="col-span-12 sm:col-span-5 bg-red-300">Second</div>
         <div className="col-span-12 sm:col-span-2 bg-pink-300">Third</div>
       </div>
